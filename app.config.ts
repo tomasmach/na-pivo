848b56ef83cc39a908bc479b02a10d9e8590478d
tomasmach <tomades1@gmail.com>
2026-06-04 21:10:14 +0200

    fix: bump iOS release version

--- app.config.ts
+++ app.config.ts
@@ -94,7 +94,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     slug: 'na-pivo',
     owner: 'tomasmach',
     scheme: 'napivo',
-    version: '1.0.0',
+    version: '1.0.1',
     icon: './assets/images/icon.png',
     orientation: 'portrait',
     userInterfaceStyle: 'dark',
