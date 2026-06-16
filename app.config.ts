98a57d6516e48e12aacc970c097c7177e5559bd2
tomasmach <tomades1@gmail.com>
2026-06-16 08:39:12 +0200

    chore: bump version to 1.2.0

--- app.config.ts
+++ app.config.ts
@@ -45,7 +45,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     slug: 'na-pivo',
     owner: 'tomasmachs-organization',
     scheme: 'napivo',
-    version: '1.1.4',
+    version: '1.2.0',
     icon: './assets/images/icon.png',
     orientation: 'portrait',
     userInterfaceStyle: 'dark',
