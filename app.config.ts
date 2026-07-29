674d5ad4051520c36ff0171835b06a4018855689
tomasmach <tomades1@gmail.com>
2026-07-29 23:15:42 +0200

    chore: bump mobile version to 2.0.0

--- app.config.ts
+++ app.config.ts
@@ -82,7 +82,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     slug: 'na-pivo',
     owner: 'tomasmachs-organization',
     scheme: 'napivo',
-    version: '1.5.0',
+    version: '2.0.0',
     icon: './assets/images/icon.png',
     orientation: 'portrait',
     userInterfaceStyle: 'automatic',
